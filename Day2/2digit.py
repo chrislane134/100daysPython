c9b5efab0fd37d712681397a8264c9dc746cb9be
chrislane134 <christopherlane101@gmail.com>
2021-02-16 11:33:15 +0000

    streamlined code, using DRY method now only 1 variable

--- Day2/2digit.py
+++ Day2/2digit.py
@@ -6,8 +6,13 @@
 
 # **Warning.** Do not change the code on lines 1-3. Your program should work for different inputs. e.g. any two-digit number.
 
+# two_digit_number = input("Type a two digit number: ")   (original effort)
+# value1 = int(two_digit_number[0])
+# value2 = int(two_digit_number[1])
+# total = value1 + value2
+# print(total)
+
+
 two_digit_number = input("Type a two digit number: ")
-value1 = int(two_digit_number[0])
-value2 = int(two_digit_number[1])
-total = value1 + value2
+total = int(two_digit_number[0]) + int(two_digit_number[1])
 print(total)
