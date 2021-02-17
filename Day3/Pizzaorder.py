cdcad2b1306f181f3c07231438d922deae348ad9
chrislane134 <christopherlane101@gmail.com>
2021-02-17 22:12:56 +0000

    Completed pizza ordering program. Using multiple if and elif statements to process a pizza order.

---
+++ Day3/Pizzaorder.py
@@ -0,0 +1,53 @@
+# Based on a user's order, work out their final bill.
+
+# ```
+# Small Pizza: $15
+# ```
+
+# ```
+# Medium Pizza: $20
+# ```
+
+# ```
+# Large Pizza: $25
+# ```
+
+# ```
+# Pepperoni for Small Pizza: +$2
+# ```
+
+# ```
+# Pepperoni for Medium or Large Pizza: +$3
+# ```
+
+# ```
+# Extra cheese for any size pizza: + $1
+
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+
+final_bill = 0
+
+
+if size == "S":
+    final_bill += 15
+    if add_pepperoni == "Y":
+        final_bill += 2
+        if extra_cheese == "Y":
+            final_bill += 1
+elif size == "M":
+    final_bill += 20
+    if add_pepperoni == "Y":
+        final_bill += 3
+        if extra_cheese == "Y":
+            final_bill += 1
+else:
+    final_bill += 25
+    if add_pepperoni == "Y":
+        final_bill += 3
+        if extra_cheese == "Y":
+            final_bill += 1
+
+print(f"Your final bill is: ${final_bill}")
