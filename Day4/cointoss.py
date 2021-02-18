b32b471d76e5efd5ad384fdcc6b10478f5ccfef5
chrislane134 <christopherlane101@gmail.com>
2021-02-18 12:39:52 +0000

    Simple coin toss program, using random module and generatign a random number.

---
+++ Day4/cointoss.py
@@ -0,0 +1,13 @@
+# You are going to write a virtual coin toss program. It will randomly tell the user "Heads" or "Tails". e.g.
+# 1 means Heads
+# 0 means Tails
+
+import random
+
+random_integer = random.randint(0, 1)
+
+
+if random_integer == 1:
+    print("Heads")
+else:
+    print("Tails")
